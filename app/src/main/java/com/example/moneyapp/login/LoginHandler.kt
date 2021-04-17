119226aac657efc3260bed3c4f2c0c49a2f7e059
Ivan Kamas <kamasivo@gmail.com>
2021-04-17 22:06:39 +0200

    deleted unsed sh.t

--- app/src/main/java/com/example/moneyapp/login/LoginHandler.kt
+++ app/src/main/java/com/example/moneyapp/login/LoginHandler.kt
@@ -10,24 +10,6 @@ import com.example.moneyapp.api.services.PostUserService
  */
 
 class LoginHandler() {
-    // in-memory cache of the loggedInUser object
-//    var user: LoggedInUser? = null
-//        private set
-
-//    val isLoggedIn: Boolean
-//        get() = user != null
-
-//    init {
-        // If user credentials will be cached in local storage, it is recommended it be encrypted
-        // @see https://developer.android.com/training/articles/keystore
-//        user = null
-//    }
-
-//    fun logout() {
-//        user = null
-//        dataSource.logout()
-//    }
-
     fun login(username: String, password: String): String {
         // handle sign up
         val apiService = PostUserService()
@@ -42,27 +24,14 @@ class LoginHandler() {
         apiService.addUser(userInfo) {
             if (it != null) {
                 Log.d("LoginRepository", it)
-                if(it == "OK") {
+                if (it == "OK") {
                     // successfully created new user
-                }
-                else {
+                } else {
                     // nepodarilo sa vytvorit pouzivatela
                 }
             }
         }
 
         return "OK"
-//        val result = dataSource.login(username, password)
-
-//        if (result is Result.Success) {
-//            setLoggedInUser(result.data)
-//        }
-//        return result
     }
-
-//    private fun setLoggedInUser(loggedInUser: LoggedInUser) {
-//        this.user = loggedInUser
-//         If user credentials will be cached in local storage, it is recommended it be encrypted
-//         @see https://developer.android.com/training/articles/keystore
-//    }
 }
