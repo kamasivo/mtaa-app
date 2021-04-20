a452f2cb39f37e67cd78334ceb9b6b91f8897d11
Ivan Kamas <kamasivo@gmail.com>
2021-04-20 21:24:32 +0200

    merge a funguje nam to

--- app/src/main/java/com/example/moneyapp/testapi/ApiDataResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moneyapp.testapi
-
-class ApiDataResponse(
-    val res: String
-)
--- app/src/main/java/com/example/moneyapp/testapi/ApiResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moneyapp.testapi
-
-class ApiResponse (
-    val data: ApiDataResponse
-)
