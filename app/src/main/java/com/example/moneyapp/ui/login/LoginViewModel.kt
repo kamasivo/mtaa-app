2dcc420118062304f1f5134a56c84f6941764fb7
Ivan Kamas <kamasivo@gmail.com>
2021-04-21 15:46:04 +0200

    my merge and login validation

--- app/src/main/java/com/example/moneyapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/moneyapp/ui/login/LoginViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.moneyapp.ui.login
 
 import android.util.Log
-import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -41,7 +40,7 @@ class LoginViewModel() : ViewModel() {
 
     fun loginDataChanged(username: String, password: String) {
         if (!isUserNameValid(username)) {
-            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_email)
         } else if (!isPasswordValid(password)) {
             _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
         } else {
@@ -51,11 +50,10 @@ class LoginViewModel() : ViewModel() {
 
     // A placeholder username validation check
     private fun isUserNameValid(username: String): Boolean {
-        return if (username.contains('@')) {
-            Patterns.EMAIL_ADDRESS.matcher(username).matches()
-        } else {
-            username.isNotBlank()
+        if(username.contains('@') && username.length > 4) {
+            return true
         }
+        return false
     }
 
     // A placeholder password validation check
