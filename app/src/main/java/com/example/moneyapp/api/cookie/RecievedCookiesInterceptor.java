054596cc8cabc4f7216b971a4fe2894f656f478a
Ivan Kamas <kamasivo@gmail.com>
2021-04-26 14:47:08 +0200

    hopefylly finaly fixed cookies

--- app/src/main/java/com/example/moneyapp/api/cookie/RecievedCookiesInterceptor.java
+++ app/src/main/java/com/example/moneyapp/api/cookie/RecievedCookiesInterceptor.java
@@ -6,9 +6,11 @@ package com.example.moneyapp.api.cookie;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.function.Predicate;
 
 import okhttp3.Interceptor;
 import okhttp3.Response;
@@ -17,15 +19,21 @@ public class RecievedCookiesInterceptor implements Interceptor {
     private Context context;
     public RecievedCookiesInterceptor(Context context) {
         this.context = context;
-    } // AddCookiesInterceptor()
+    }
     @Override
     public Response intercept(Chain chain) throws IOException {
         Response originalResponse = chain.proceed(chain.request());
-
         if (!originalResponse.headers("Set-Cookie").isEmpty()) {
-            HashSet<String> cookies = (HashSet<String>) PreferenceManager.getDefaultSharedPreferences(context).getStringSet("PREF_COOKIES", new HashSet<String>());
+//            PreferenceManager.getDefaultSharedPreferences(context).edit().clear();
+            PreferenceManager.getDefaultSharedPreferences(context).getStringSet("PREF_COOKIES", new HashSet<String>()).clear();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.clear();
+            editor.apply();
 
+            HashSet<String> cookies = (HashSet<String>) PreferenceManager.getDefaultSharedPreferences(context).getStringSet("PREF_COOKIES", new HashSet<String>());
             for (String header : originalResponse.headers("Set-Cookie")) {
+//                Log.d("cookies setting", header);
                 cookies.add(header);
             }
 
