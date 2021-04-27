92a9c7a0a07e5f798d5138a550463708500bc1ba
Ivan Kamas <kamasivo@gmail.com>
2021-04-27 17:17:00 +0200

    choose billu funguje

--- app/src/main/java/com/example/moneyapp/ui/create_income/FragmentCreateIncome.kt
+++ app/src/main/java/com/example/moneyapp/ui/create_income/FragmentCreateIncome.kt
@@ -7,10 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -22,7 +19,8 @@ import com.example.moneyapp.databinding.FragmentCreateIncomeBinding
 class CreateIncome : Fragment(){
     private lateinit var model: CreateIncomeViewModel
     private var _binding: FragmentCreateIncomeBinding? = null
-    private lateinit var spinner: Spinner
+    private lateinit var spinnerIncome: Spinner
+    private lateinit var spinnerBill: Spinner
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -35,32 +33,39 @@ class CreateIncome : Fragment(){
         val view = binding.root
         model.loadBills()
 
+        setObservers()
+        return view
+    }
+
+    fun setObservers() {
         model.listOfBills.observe(viewLifecycleOwner, Observer {
             it?.let {
-                Log.d("FragmentCreateIncome", it.toString())
-                val items: ArrayList<String> = ArrayList()
+                val items: ArrayList<Item> = ArrayList()
 
                 for (i in it) {
                     Log.d("FragmentCreateIncome", i.name.toString())
+                    val it = Item((i.id.toString().toInt()), i.name.toString())
+                    items.add(it)
                 }
-                val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+
+                val adapter: ArrayAdapter<Item> = ArrayAdapter<Item>(
                     GlobalApplication.appContext!!,
                     android.R.layout.simple_spinner_item,
                     items
                 )
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
 
-                spinner.adapter = adapter
+                spinnerBill.adapter = adapter
             }
         })
-        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val sum = binding.sumIncome
         val loading = binding.loading
         val create = binding.createIncome
-        spinner = binding.incomeSource
+        spinnerIncome = binding.incomeSource
+        spinnerBill = binding.billChoose
 
         val model: CreateIncomeViewModel by viewModels()
 
@@ -68,9 +73,6 @@ class CreateIncome : Fragment(){
             val createIncomeState = it ?: return@Observer
 
             // disable login button unless both username / password is valid
-            create.isEnabled = createIncomeState.isDataValid
-
-
             create.isEnabled = createIncomeState.isDataValid
 
             if (createIncomeState.sumError != null) {
@@ -99,6 +101,8 @@ class CreateIncome : Fragment(){
 
 
         create.setOnClickListener {
+            val billChoose: Item = spinnerBill.selectedItem as Item
+            Log.d("billChoosen", billChoose.id.toString())
             loading.visibility = View.VISIBLE
 //            model.createIncome(sum.text.toString().toInt())
         }
@@ -106,6 +110,12 @@ class CreateIncome : Fragment(){
 
     private fun showCreateIncomeFailed(errorString: String) {
         Toast.makeText(this.context, errorString, Toast.LENGTH_SHORT).show()
+    }
+}
+
+class Item (val id: Int, val name: String){
+    override fun toString(): String {
+        return name;
     }
 }
 
