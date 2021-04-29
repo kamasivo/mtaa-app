96a5ba6857d7bfe7c4fff937d5d967783bd8aa36
Ivan Kamas <kamasivo@gmail.com>
2021-04-29 13:48:05 +0200

    fix add exp

--- app/src/main/java/com/example/moneyapp/api/models/ExpenditureCategoryArray.kt
+++ app/src/main/java/com/example/moneyapp/api/models/ExpenditureCategoryArray.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class ExpenditureCategoryArray (
-    @SerializedName("expenditureCategory")
+    @SerializedName("expenditureCategories")
     @Expose
     val expenditureCategories: List<ExpenditureCategory>
 )
